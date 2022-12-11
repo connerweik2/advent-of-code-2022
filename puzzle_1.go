0200bd6957bbd8aaa292c561df1073d213201711
Conner Weik <connerweik1@gmail.com>
2022-12-11 14:37:44 -0500

    Go solutions for Day 2!

--- puzzle_1.go
+++ puzzle_1.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"strconv"
 )
 
 func main() {
@@ -15,27 +14,34 @@ func main() {
 	}
 	defer file.Close()
 
-	currentSum := 0
-	max := 0
+	resultScore := 0
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		thisLine, err := strconv.Atoi(scanner.Text())
-		if err != nil {
-			if currentSum > max {
-				max = currentSum
-			}
-			currentSum = 0
-		} else {
-			currentSum += thisLine
+		thisLine := scanner.Text()
+
+		// The opponent's choice and our choice.
+		// 0 for rock, 1 for paper, 2 for scissors.
+		opponentChoice := int(thisLine[0] - 'A')
+		myChoice := int(thisLine[2] - 'X')
+
+		// Update the result score based on our choice:
+		// 1 for rock, 2 for paper, 3 for scissors.
+		resultScore += myChoice + 1
+
+		// For a draw, add 3.
+		// For a win, add 6.
+		// In a win, our choice will be
+		// one to the right of the opponent's in the list
+		// [rock, paper, scissors], wrapping around at the ends.
+		if opponentChoice == myChoice {
+			resultScore += 3
+		} else if (opponentChoice+1)%3 == myChoice {
+			resultScore += 6
 		}
 	}
 
-	if currentSum > max {
-		max = currentSum
-	}
-
-	fmt.Println(max)
+	fmt.Println(resultScore)
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
--- puzzle_2.go
+++ puzzle_2.go
@@ -5,8 +5,6 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"sort"
-	"strconv"
 )
 
 func main() {
@@ -16,35 +14,34 @@ func main() {
 	}
 	defer file.Close()
 
-	currentSum := 0
-	top3 := []int{-1, -1, -1}
+	resultScore := 0
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		thisLine, err := strconv.Atoi(scanner.Text())
-		if err != nil {
-			if currentSum > top3[0] {
-				top3[0] = currentSum
-				sort.Ints(top3)
-			}
-			currentSum = 0
-		} else {
-			currentSum += thisLine
+		thisLine := scanner.Text()
+
+		// The opponent's choice: 0 for rock, 1 for paper, 2 for scissors.
+		// And the round result: 0 for loss, 1 for draw, 2 for win.
+		opponentChoice := int(thisLine[0] - 'A')
+		roundResult := int(thisLine[2] - 'X')
+
+		// Update the result score based on the round result: 3 for draw, 6 for win.
+		if roundResult == 1 {
+			resultScore += 3
 		}
+		if roundResult == 2 {
+			resultScore += 6
+		}
+
+		// Based on the round result, we know which we will pick:
+		// if loss, pick 1 to the left of the opponent in [rock, paper, scissors],
+		// wrapping around at the ends; if draw, pick the same as the
+		// opponent; if win, pick 1 to the right. Update score accordingly:
+		// 1 for rock, 2 for paper, 3 for scissors.
+		resultScore += (opponentChoice+roundResult-1+3)%3 + 1
 	}
 
-	if currentSum > top3[0] {
-		top3[0] = currentSum
-		sort.Ints(top3)
-	}
-
-	resultSum := 0
-
-	for i := 0; i < 3; i++ {
-		resultSum += top3[i]
-	}
-
-	fmt.Println(resultSum)
+	fmt.Println(resultScore)
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
