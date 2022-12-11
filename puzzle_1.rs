f4211001b3ab2fe877806e003494732d01e49c0d
Conner Weik <connerweik1@gmail.com>
2022-12-11 14:11:33 -0500

    commit

--- puzzle_1.rs
+++ puzzle_1.rs
@@ -1,46 +1,36 @@
 use std::fs::File;
 use std::io::{self, BufRead};
-use std::collections::BinaryHeap;
-use std::cmp::Reverse;
-
 
 fn main() {
     let input_file = File::open("./input.txt").expect("Failed to open input file");
 
-    let mut heap = BinaryHeap::new();
-    heap.push(Reverse(-1));
-    heap.push(Reverse(-1));
-    heap.push(Reverse(-1));
-
-    let mut current_sum: i32 = 0;
+    let mut result_score: i32 = 0;
 
     let lines = io::BufReader::new(input_file).lines();
     for line in lines {
-        let line_unwrapped = line.unwrap();
-        let parse_result = line_unwrapped.parse::<i32>();
-        if parse_result.is_ok() {
-            current_sum += parse_result.unwrap();
-        } else {
-            let Reverse(peek_value) = *heap.peek().unwrap();
-            if current_sum > peek_value {
-                heap.pop();
-                heap.push(Reverse(current_sum));
-            }
-            current_sum = 0;
+        let line_unwrap = line.unwrap();
+        let line_bytes = line_unwrap.as_bytes();
+
+        // The opponent's choice and our choice.
+        // 0 for rock, 1 for paper, 2 for scissors.
+        let opponent_choice = line_bytes[0] as i32 - 'A' as i32;
+        let my_choice = line_bytes[2] as i32 - 'X' as i32;
+
+        // Update the result score based on our choice:
+        // 1 for rock, 2 for paper, 3 for scissors.
+        result_score += my_choice + 1;
+
+        // Draw: add 3.
+        if opponent_choice == my_choice {
+            result_score += 3;
+        }
+        // Win: Add 6. In a win, our choice will be
+        // one to the right of the opponent's in the list
+        // [rock, paper, scissors], wrapping around at the ends.
+        else if (opponent_choice + 1) % 3 == my_choice {
+            result_score += 6;
         }
     }
 
-    let Reverse(peek_value) = *heap.peek().unwrap();
-    if current_sum > peek_value {
-        heap.pop();
-        heap.push(Reverse(current_sum));
-    }
-
-    let mut result_sum = 0;
-    while let Some(Reverse(pop_value)) = heap.pop() {
-        result_sum += pop_value;
-    }
-
-    println!("{result_sum}");
-
+    println!("{result_score}");
 }
--- puzzle_2.rs
+++ puzzle_2.rs
@@ -5,27 +5,28 @@ fn main() {
     let input_file = File::open("./input.txt").expect("Failed to open input file");
 
     let mut result_score: i32 = 0;
-    let mut current_sum: i32 = 0;
 
     let lines = io::BufReader::new(input_file).lines();
     for line in lines {
-        let line_bytes = line.as_bytes();
-        let opponent_choice: i32 = 
-        let opponent_choice
-        if parse_result.is_ok() {
-            current_sum += parse_result.unwrap();
-        } else {
-            if current_sum > max {
-                max = current_sum;
-            }
-            current_sum = 0;
-        }
+        let line_unwrap = line.unwrap();
+        let line_bytes = line_unwrap.as_bytes();
+
+        // The opponent's choice: 0 for rock, 1 for paper, 2 for scissors.
+        // And the round result: 0 for loss, 1 for draw, 2 for win.
+        let opponent_choice = line_bytes[0] as i32 - 'A' as i32;
+        let round_result = line_bytes[2] as i32 - 'X' as i32;
+
+        // Update the result score based on the round result.
+        if round_result == 1 {result_score += 3;}
+        if round_result == 2 {result_score += 6;}
+
+        // Based on the round result, we know which we will pick:
+        // if loss, pick 1 to the left of the opponent in [rock, paper, scissors],
+        // wrapping around at the ends; if draw, pick the same as the
+        // opponent; if win, pick 1 to the right. Update score accordingly:
+        // 1 for rock, 2 for paper, 3 for scissors.
+        result_score += (opponent_choice + round_result - 1 + 3) % 3 + 1;
     }
 
-    if current_sum > max {
-        max = current_sum;
-    }
-
-    println!("{max}");
-
+    println!("{result_score}");
 }
