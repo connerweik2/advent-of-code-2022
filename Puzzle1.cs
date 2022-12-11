62e4dcd3eeddd8181e47ad2666521e82eda6f811
Conner Weik <connerweik1@gmail.com>
2022-12-11 01:40:10 -0500

    commit

--- Puzzle1.cs
+++ Puzzle1.cs
@@ -1,3 +1,27 @@
-static void Main(string[] args) {
-    Console.WriteLine("Hello world!");
-}
+// int max = 0;
+// int currentSum = 0;
+// using (StreamReader sr = File.OpenText("./input.txt"))
+// {
+//     string thisLine;
+//     while ((thisLine = sr.ReadLine()) != null)
+//     {
+
+//         Console.WriteLine(thisLine);
+//         int thisLineInt = 0;
+//         bool result = int.TryParse(thisLine, out thisLineInt);
+//         if (result)
+//         {
+//             currentSum += thisLineInt;
+//         }
+//         else
+//         {
+//             if (currentSum > max)
+//             {
+//                 max = currentSum;
+//             }
+//             currentSum = 0;
+//         }
+//     }
+
+//     Console.WriteLine(currentSum);
+// }
---
+++ Puzzle2.cs
@@ -0,0 +1,32 @@
+﻿int currentSum = 0;
+PriorityQueue<int, int> pq = new PriorityQueue<int, int>();
+pq.Enqueue(-1, -1);
+pq.Enqueue(-1, -1);
+pq.Enqueue(-1, -1);
+
+using (StreamReader sr = File.OpenText("./input.txt"))
+{
+    string thisLine;
+    while ((thisLine = sr.ReadLine()) != null)
+    {
+
+        Console.WriteLine(thisLine);
+        int thisLineInt = 0;
+        bool result = int.TryParse(thisLine, out thisLineInt);
+        if (result)
+        {
+            currentSum += thisLineInt;
+        }
+        else
+        {
+            if (currentSum > pq.Peek())
+            {
+                pq.Dequeue();
+                pq.Enqueue(currentSum, currentSum);
+            }
+            currentSum = 0;
+        }
+    }
+
+    for (int i = 0; i < )
+}
