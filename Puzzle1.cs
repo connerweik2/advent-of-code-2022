710bffd03210f8396cc58e74b8284590a03f00fc
Conner Weik <connerweik1@gmail.com>
2022-12-11 11:17:48 -0500

    Update Puzzle1.cs

--- Puzzle1.cs
+++ Puzzle1.cs
@@ -1,27 +1,31 @@
-int max = 0;
-int currentSum = 0;
-using (StreamReader sr = File.OpenText("./input.txt"))
-{
-    string thisLine;
-    while ((thisLine = sr.ReadLine()) != null)
-    {
+// int max = 0;
+// int currentSum = 0;
+// using (StreamReader sr = File.OpenText("./input.txt"))
+// {
+//     string thisLine;
+//     while ((thisLine = sr.ReadLine()) != null)
+//     {
 
-        Console.WriteLine(thisLine);
-        int thisLineInt = 0;
-        bool result = int.TryParse(thisLine, out thisLineInt);
-        if (result)
-        {
-            currentSum += thisLineInt;
-        }
-        else
-        {
-            if (currentSum > max)
-            {
-                max = currentSum;
-            }
-            currentSum = 0;
-        }
-    }
+//         int thisLineInt = 0;
+//         bool result = int.TryParse(thisLine, out thisLineInt);
+//         if (result)
+//         {
+//             currentSum += thisLineInt;
+//         }
+//         else
+//         {
+//             if (currentSum > max)
+//             {
+//                 max = currentSum;
+//             }
+//             currentSum = 0;
+//         }
+//     }
 
-    Console.WriteLine(currentSum);
-}
+//     if (currentSum > max)
+//     {
+//         max = currentSum;
+//     }
+
+//     Console.WriteLine(max);
+// }
