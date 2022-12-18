f9bde18c2b0baa48153fdd370b7d2be5e2f021cc
Conner Weik <connerweik1@gmail.com>
2022-12-18 17:28:33 -0500

    Java solutions for Day 5

--- Puzzle2.java
+++ Puzzle2.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Puzzle2 {
 
@@ -8,24 +9,49 @@ public class Puzzle2 {
         File inputFile = new File("./input.txt");
         Scanner in = new Scanner(inputFile);
 
-        int result = 0;
-
-        while (in.hasNextLine()) {
-            String lineTrimmed = in.nextLine().trim();
-            String[] lineSplit = lineTrimmed.split(",");
-            int firstMin, firstMax, secondMin, secondMax;
-            firstMin = Integer.parseInt(lineSplit[0].substring(0, lineSplit[0].indexOf("-")));
-            firstMax = Integer.parseInt(lineSplit[0].substring(lineSplit[0].indexOf("-") + 1));
-            secondMin = Integer.parseInt(lineSplit[1].substring(0, lineSplit[1].indexOf("-")));
-            secondMax = Integer.parseInt(lineSplit[1].substring(lineSplit[1].indexOf("-") + 1));
-
-            if (firstMin <= secondMax && firstMax >= secondMin) {
-                    result++;
+        Stack<Character>[] stacks = new Stack[9];
+        for (int i = 0; i < 9; i++) {
+            stacks[i] = new Stack<>();
+        }
+        char[][] initialStacks = new char[8][9];
+        for (int i = 0; i < 8; i++) {
+            String line = in.nextLine();
+            for (int j = 0; j < 9; j++) {
+                initialStacks[i][j] = line.charAt(j * 4 + 1);
             }
         }
 
-        in.close();
+        for (int i = 7; i >= 0; i--) {
+            for (int j = 0; j < 9; j++) {
+                if (initialStacks[i][j] != ' ') {
+                    stacks[j].push(initialStacks[i][j]);
+                }
+            }
+        }
 
-        System.out.println(result);
+        in.nextLine();
+        in.nextLine();
+
+        while (in.hasNextLine()) {
+            String[] split = in.nextLine().split(" ");
+            int moveCount = Integer.parseInt(split[1]);
+            int stackFrom = Integer.parseInt(split[3]) - 1;
+            int stackTo = Integer.parseInt(split[5]) - 1;
+            Stack<Character> tempStack = new Stack<>();
+            for (int i = 0; i < moveCount; i++) {
+                tempStack.push(stacks[stackFrom].pop());
+            }
+            for (int i = 0; i < moveCount; i++) {
+                stacks[stackTo].push(tempStack.pop());
+            }
+        }
+
+        
+        in.close();
+        
+        for (int i = 0; i < 9; i++) {
+            System.out.print(stacks[i].pop());
+        }
+        System.out.println();
     }
 }
