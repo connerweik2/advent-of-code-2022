bf40f3c800d548d806408e7f3442675ede9cae82
Conner Weik <connerweik1@gmail.com>
2022-12-18 22:15:38 -0500

    Rust solutions for Day 5

--- puzzle_1.rs
+++ puzzle_1.rs
@@ -4,24 +4,47 @@ use std::io::{self, BufRead};
 fn main() {
     let input_file = File::open("./input.txt").expect("Failed to open input file");
 
-    let mut result: i32 = 0;
+    let mut lines = io::BufReader::new(input_file).lines();
 
-    let lines = io::BufReader::new(input_file).lines();
-    for line in lines {
-        let line_unwrap = line.unwrap();
-        let split = line_unwrap.split(",").collect::<Vec<&str>>();
-        let first_split = split[0].split("-").collect::<Vec<&str>>();
-        let second_split = split[1].split("-").collect::<Vec<&str>>();
-        let first_min = first_split[0].parse::<i32>().unwrap();
-        let first_max = first_split[1].parse::<i32>().unwrap();
-        let second_min = second_split[0].parse::<i32>().unwrap();
-        let second_max = second_split[1].parse::<i32>().unwrap();
+    let mut stacks = [(); 9].map(|_| Vec::new());
 
-        if (first_min >= second_min && first_max <= second_max) ||
-           (second_min >= first_min && second_max <= first_max) {
-            result += 1;
+    let mut initial_state = [[' ' as u8; 9]; 8];
+
+    for i in 0..8 {
+        let line = lines.next().unwrap().unwrap();
+        let line_bytes = line.as_bytes();
+        for j in 0..9 {
+            initial_state[i][j] = line_bytes[j * 4 + 1];
         }
     }
 
-    println!("{result}");
+    for i in (0..8).rev() {
+        for j in 0..9 {
+            if initial_state[i][j] != ' ' as u8 {
+                stacks[j].push(initial_state[i][j]);
+            }
+        }
+    }
+
+    lines.next();
+    lines.next();
+
+    for line in lines {
+        let line_unwrapped = line.unwrap();
+        let line_split = line_unwrapped.split(" ").collect::<Vec<&str>>();
+        
+        let num_moves = line_split[1].parse::<i32>().unwrap();
+        let from_stack = line_split[3].parse::<i32>().unwrap() as usize - 1;
+        let to_stack = line_split[5].parse::<i32>().unwrap() as usize - 1;
+
+        for _ in 0..num_moves {
+            let popped_val = stacks[from_stack].pop().unwrap();
+            stacks[to_stack].push(popped_val);
+        }
+    }
+
+    for i in 0..9 {
+        print!("{}", stacks[i].pop().unwrap() as char);
+    }
+    println!();
 }
