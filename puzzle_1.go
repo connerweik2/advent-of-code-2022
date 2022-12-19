7589a572b99164577324a958b65732684d1917ee
Conner Weik <connerweik1@gmail.com>
2022-12-18 23:17:44 -0500

    Go solutions for Day 5

--- puzzle_1.go
+++ puzzle_1.go
@@ -5,8 +5,8 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"regexp"
 	"strconv"
+	"strings"
 )
 
 func main() {
@@ -16,25 +16,51 @@ func main() {
 	}
 	defer file.Close()
 
-	result := 0
-	re := regexp.MustCompile(",|-")
+	var stacks [9][]uint8
+
+	var initialState [8][9]uint8
 
 	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		line := (scanner.Text())
-		split := re.Split(line, -1)
-		firstMin, _ := strconv.Atoi(split[0])
-		firstMax, _ := strconv.Atoi(split[1])
-		secondMin, _ := strconv.Atoi(split[2])
-		secondMax, _ := strconv.Atoi(split[3])
-
-		if firstMin >= secondMin && firstMax <= secondMax ||
-			secondMin >= firstMin && secondMax <= firstMax {
-			result++
+	for i := 0; i < 8; i++ {
+		scanner.Scan()
+		line := scanner.Text()
+		for j := 0; j < 9; j++ {
+			initialState[i][j] = line[j*4+1]
 		}
 	}
 
-	fmt.Println(result)
+	for i := 7; i >= 0; i-- {
+		for j := 0; j < 9; j++ {
+			if initialState[i][j] != ' ' {
+				stacks[j] = append(stacks[j], initialState[i][j])
+			}
+		}
+	}
+
+	scanner.Scan()
+	scanner.Scan()
+
+	for scanner.Scan() {
+		split := strings.Split(scanner.Text(), " ")
+
+		numMoves, _ := strconv.Atoi(split[1])
+		stackFrom, _ := strconv.Atoi(split[3])
+		stackFrom -= 1
+		stackTo, _ := strconv.Atoi(split[5])
+		stackTo -= 1
+
+		for i := 0; i < numMoves; i++ {
+			stacks[stackTo] = append(stacks[stackTo], stacks[stackFrom][len(stacks[stackFrom])-1])
+			stacks[stackFrom] = stacks[stackFrom][:len(stacks[stackFrom])-1]
+		}
+	}
+
+	var bytesResult []byte
+	for i := 0; i < 9; i++ {
+		bytesResult = append(bytesResult, stacks[i][len(stacks[i])-1])
+	}
+
+	fmt.Println(string(bytesResult))
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
