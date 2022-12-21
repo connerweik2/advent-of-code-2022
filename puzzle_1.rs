3b70092c901f98b53ac3709148b5a172545a3451
Conner Weik <connerweik1@gmail.com>
2022-12-20 19:08:24 -0500

    Rust solutions for day 6

--- puzzle_1.rs
+++ puzzle_1.rs
@@ -4,47 +4,32 @@ use std::io::{self, BufRead};
 fn main() {
     let input_file = File::open("./input.txt").expect("Failed to open input file");
 
-    let mut lines = io::BufReader::new(input_file).lines();
+    let _input = io::BufReader::new(input_file).lines().next().unwrap().unwrap();
+    let input = _input.as_bytes();
 
-    let mut stacks = [(); 9].map(|_| Vec::new());
+    let mut freq = [0; 500];
 
-    let mut initial_state = [[' ' as u8; 9]; 8];
-
-    for i in 0..8 {
-        let line = lines.next().unwrap().unwrap();
-        let line_bytes = line.as_bytes();
-        for j in 0..9 {
-            initial_state[i][j] = line_bytes[j * 4 + 1];
-        }
+    for i in 0..3 {
+        freq[input[i as usize] as usize] += 1;
     }
 
-    for i in (0..8).rev() {
-        for j in 0..9 {
-            if initial_state[i][j] != ' ' as u8 {
-                stacks[j].push(initial_state[i][j]);
+    for i in 3..input.len() {
+        freq[input[i as usize] as usize] += 1;
+
+        let mut all_unique = true;
+
+        for j in 0..256 {
+            if freq[j] > 1 {
+                all_unique = false;
+                break;
             }
         }
-    }
 
-    lines.next();
-    lines.next();
-
-    for line in lines {
-        let line_unwrapped = line.unwrap();
-        let line_split = line_unwrapped.split(" ").collect::<Vec<&str>>();
-        
-        let num_moves = line_split[1].parse::<i32>().unwrap();
-        let from_stack = line_split[3].parse::<i32>().unwrap() as usize - 1;
-        let to_stack = line_split[5].parse::<i32>().unwrap() as usize - 1;
-
-        for _ in 0..num_moves {
-            let popped_val = stacks[from_stack].pop().unwrap();
-            stacks[to_stack].push(popped_val);
+        if all_unique {
+            println!("{}", i + 1);
+            break;
         }
-    }
 
-    for i in 0..9 {
-        print!("{}", stacks[i].pop().unwrap() as char);
+        freq[input[(i - 3) as usize] as usize] -= 1
     }
-    println!();
 }
--- puzzle_2.rs
+++ puzzle_2.rs
@@ -4,54 +4,32 @@ use std::io::{self, BufRead};
 fn main() {
     let input_file = File::open("./input.txt").expect("Failed to open input file");
 
-    let mut lines = io::BufReader::new(input_file).lines();
+    let _input = io::BufReader::new(input_file).lines().next().unwrap().unwrap();
+    let input = _input.as_bytes();
 
-    let mut stacks = [(); 9].map(|_| Vec::new());
+    let mut freq = [0; 500];
 
-    let mut initial_state = [[' ' as u8; 9]; 8];
-
-    for i in 0..8 {
-        let line = lines.next().unwrap().unwrap();
-        let line_bytes = line.as_bytes();
-        for j in 0..9 {
-            initial_state[i][j] = line_bytes[j * 4 + 1];
-        }
+    for i in 0..13 {
+        freq[input[i as usize] as usize] += 1;
     }
 
-    for i in (0..8).rev() {
-        for j in 0..9 {
-            if initial_state[i][j] != ' ' as u8 {
-                stacks[j].push(initial_state[i][j]);
+    for i in 13..input.len() {
+        freq[input[i as usize] as usize] += 1;
+
+        let mut all_unique = true;
+
+        for j in 0..256 {
+            if freq[j] > 1 {
+                all_unique = false;
+                break;
             }
         }
-    }
 
-    lines.next();
-    lines.next();
-
-    for line in lines {
-        let line_unwrapped = line.unwrap();
-        let line_split = line_unwrapped.split(" ").collect::<Vec<&str>>();
-        
-        let num_moves = line_split[1].parse::<i32>().unwrap();
-        let from_stack = line_split[3].parse::<i32>().unwrap() as usize - 1;
-        let to_stack = line_split[5].parse::<i32>().unwrap() as usize - 1;
-
-        let mut temp_stack = Vec::<u8>::new();
-
-        for _ in 0..num_moves {
-            let popped_val = stacks[from_stack].pop().unwrap();
-            temp_stack.push(popped_val);
+        if all_unique {
+            println!("{}", i + 1);
+            break;
         }
 
-        for _ in 0..num_moves {
-            let popped_val = temp_stack.pop().unwrap();
-            stacks[to_stack].push(popped_val);
-        }
+        freq[input[(i - 13) as usize] as usize] -= 1
     }
-
-    for i in 0..9 {
-        print!("{}", stacks[i].pop().unwrap() as char);
-    }
-    println!();
 }
