dea7df403ecd51d351d707d62486b88b29bc6da5
Conner Weik <connerweik1@gmail.com>
2022-12-22 17:00:35 -0500

    Python solutions for day 7

--- puzzle_1.py
+++ puzzle_1.py
@@ -1,20 +1,49 @@
-input_string = None
+class Node:
+    def __init__(self, parent, is_dir, size):
+        self.parent = parent
+        self.is_dir = is_dir
+        self.size = size
+        self.children = {}
+
+lines = []
 
 with open('./input.txt', 'r') as f:
-    input_string = [ord(c) for c in f.readline()]
+    lines = f.readlines()
 
-freq = [0] * 256
+lines = [line.strip() for line in lines]
 
-for i in range(3):
-    freq[input_string[i]] += 1
+root = Node(None, True, 0)
+current_node = root
 
-for i in range(3, len(input_string)):
-    freq[input_string[i]] += 1
-    all_unique = True
-    for j in range(256):
-        if freq[j] > 1:
-            all_unique = False
-    if all_unique:
-        print(i + 1)
+i = 1
+while i < len(lines):
+    split = lines[i].split(' ')
+    i += 1
+    if i == len(lines):
         break
-    freq[input_string[i - 3]] -= 1
+    if split[1] == 'ls':
+        while i < len(lines) and '$' not in lines[i]:
+            split = lines[i].split(' ')
+            if split[0] == 'dir':
+                current_node.children[split[1]] = Node(current_node, True, 0)
+            else:
+                current_node.children[split[1]] = Node(current_node, False, int(split[0]))
+            i += 1
+    elif split[1] == 'cd':
+        if split[2] == '..':
+            current_node = current_node.parent
+        else:
+            current_node = current_node.children[split[2]]
+
+dir_sizes = []
+
+def set_dir_sizes(root):
+    if not root.is_dir:
+        return root.size
+    root.size = sum([set_dir_sizes(child) for child in root.children.values()])
+    dir_sizes.append(root.size)
+    return root.size
+
+set_dir_sizes(root)
+
+print(sum([dir_size for dir_size in dir_sizes if dir_size <= 100000]))
