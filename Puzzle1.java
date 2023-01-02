78d4ce931e61f03b3cf1a87694410a3e6573de73
Conner Weik <connerweik1@gmail.com>
2023-01-02 15:35:41 -0500

    update java; python puzzle1

--- Puzzle1.java
+++ Puzzle1.java
@@ -6,24 +6,28 @@ import java.util.Scanner;
 public class Puzzle1 {
     public static boolean visible(int targetRow, int targetCol, int[][] grid) {
         boolean visibleUp = true, visibleDown = true, visibleLeft = true, visibleRight = true;
-        for (int row = targetRow - 1; row >= 0; row--) {
+        for (int row = 0; row < targetRow; row++) {
             if (grid[row][targetCol] >= grid[targetRow][targetCol]) {
                 visibleUp = false;
+                break;
             }
         }
         for (int row = targetRow + 1; row < grid.length; row++) {
             if (grid[row][targetCol] >= grid[targetRow][targetCol]) {
                 visibleDown = false;
+                break;
             }
         }
-        for (int col = targetCol - 1; col >= 0; col--) {
+        for (int col = 0; col < targetCol; col++) {
             if (grid[targetRow][col] >= grid[targetRow][targetCol]) {
                 visibleLeft = false;
+                break;
             }
         }
         for (int col = targetCol + 1; col < grid[0].length; col++) {
             if (grid[targetRow][col] >= grid[targetRow][targetCol]) {
                 visibleRight = false;
+                break;
             }
         }
         return visibleUp || visibleDown || visibleLeft || visibleRight;
--- puzzle_1.py
+++ puzzle_1.py
@@ -3,3 +3,33 @@ lines = []
 with open('./input.txt', 'r') as f:
     lines = [line.strip() for line in f.readlines()]
 
+grid = [[ord(c) - ord('0') for c in line] for line in lines]
+
+def visible(target_row, target_col):
+    visible_up, visible_down, visible_left, visible_right = True, True, True, True
+    for row in range(target_row):
+        if grid[row][target_col] >= grid[target_row][target_col]:
+            visible_up = False
+            break
+    for row in range(target_row + 1, len(grid)):
+        if grid[row][target_col] >= grid[target_row][target_col]:
+            visible_down = False
+            break
+    for col in range(target_col):
+        if grid[target_row][col] >= grid[target_row][target_col]:
+            visible_left = False
+            break
+    for col in range(target_col + 1, len(grid[0])):
+        if grid[target_row][col] >= grid[target_row][target_col]:
+            visible_right = False
+            break
+    return visible_up or visible_down or visible_left or visible_right
+
+result = 0
+
+for row in range(len(grid)):
+    for col in range(len(grid[0])):
+        if visible(row, col):
+            result += 1
+            
+print(result)
