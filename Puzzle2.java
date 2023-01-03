26a9a64fa0615d8b6057696c350f6cc39f9ce4ca
Conner <connerweik1@gmail.com>
2023-01-03 16:52:12 -0500

    java 2 solution for day 9

--- Puzzle2.java
+++ Puzzle2.java
@@ -1,69 +1,70 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
+import java.awt.Point;
 
 public class Puzzle2 {
-    public static int scenicScore(int targetRow, int targetCol, int[][] grid) {
-        int distanceUp = 0, distanceDown = 0, distanceLeft = 0, distanceRight = 0;
-        for (int row = targetRow - 1; row >= 0; row--) {
-            distanceUp++;
-            if (grid[row][targetCol] >= grid[targetRow][targetCol]) {
-                break;
-            }
-        }
-        for (int row = targetRow + 1; row < grid.length; row++) {
-            distanceDown++;
-            if (grid[row][targetCol] >= grid[targetRow][targetCol]) {
-                break;
-            }
-        }
-        for (int col = targetCol - 1; col >= 0; col--) {
-            distanceLeft++;
-            if (grid[targetRow][col] >= grid[targetRow][targetCol]) {
-                break;
-            }
-        }
-        for (int col = targetCol + 1; col < grid[0].length; col++) {
-            distanceRight++;
-            if (grid[targetRow][col] >= grid[targetRow][targetCol]) {
-                break;
-            }
-        }
-        return distanceUp * distanceDown * distanceLeft * distanceRight;
-    }
-
     public static void main(String[] args) throws FileNotFoundException {
         ArrayList<String> lines = new ArrayList<>();
 
-        File inputFile = new File("./input.txt");
+        File inputFile = new File("input.txt");
         Scanner in = new Scanner(inputFile);
 
         while (in.hasNext()) {
-            lines.add(in.next());
+            lines.add(in.nextLine());
         }
 
         in.close();
 
-        int[][] grid = new int[lines.size()][lines.get(0).length()];
+        Set<Point> visited = new HashSet<>();
 
-        for (int row = 0; row < grid.length; row++) {
-            for (int col = 0; col < grid[0].length; col++) {
-                grid[row][col] = (int)(lines.get(row).charAt(col) - '0');
+        ArrayList<Point> rope = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            rope.add(new Point(0, 0));
+        }
+        
+        visited.add(new Point(0, 0));
+
+        for (int i = 0; i < lines.size(); i++) {
+            String[] split = lines.get(i).split(" ");
+            char direction = split[0].charAt(0);
+            int distance = Integer.parseInt(split[1]);
+            for (int j = 0; j < distance; j++) {
+                processMove(rope, direction, visited);
             }
         }
+        
+        System.out.println(visited.size());
+    }
 
-        int result = 0;
-
-        for (int targetRow = 0; targetRow < grid.length; targetRow++) {
-            for (int targetCol = 0; targetCol < grid[0].length; targetCol++) {
-                int score = scenicScore(targetRow, targetCol, grid);
-                if (score > result) {
-                    result = score;
-                }
+    public static void processMove(ArrayList<Point> rope, char direction, Set<Point> visited) {
+        if (direction == 'U') {
+            rope.get(0).y--;
+        } else if (direction == 'D') {
+            rope.get(0).y++;
+        } else if (direction == 'L') {
+            rope.get(0).x--;
+        } else if (direction == 'R') {
+            rope.get(0).x++;
+        }
+        for (int i = 1; i < rope.size(); i++) {
+            if (Math.abs(rope.get(i - 1).x - rope.get(i).x) <= 1 && Math.abs(rope.get(i - 1).y - rope.get(i).y) <= 1) {
+                return;
+            }
+            if (rope.get(i - 1).x > rope.get(i).x) {
+                rope.get(i).x++;
+            } else if (rope.get(i - 1).x < rope.get(i).x) {
+                rope.get(i).x--;
+            }
+            if (rope.get(i - 1).y > rope.get(i).y) {
+                rope.get(i).y++;
+            } else if (rope.get(i - 1).y < rope.get(i).y) {
+                rope.get(i).y--;
             }
         }
-
-        System.out.println(result);
+        visited.add(new Point(rope.get(rope.size() - 1).x, rope.get(rope.size() - 1).y));
     }
 }
